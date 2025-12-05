292d8c3d01d68c38cca6728827afb7d3fbba2f9b
Rafique Ahamad <rafiqueahamad1818@gmail.com>
2025-12-05 16:36:50 +0530

    first

---
+++ frontEnd/src/components/Freebook.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import list from '../../public/list.json'
+function Freebook() {
+    const filterData= list.filter((data)=>  data.category === "free");
+    console.log(filterData);
+    
+  return (
+    <div></div>
+  )
+}
+
+export default Freebook
